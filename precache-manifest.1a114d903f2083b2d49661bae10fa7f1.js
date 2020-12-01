39d32a7a0c5bcd09d0c374276c074e303fee0efd
Shaun-Farrell <shaunfarrell@outlook.com>
2020-12-01 17:31:45 +0000

    Updates

---
+++ precache-manifest.1a114d903f2083b2d49661bae10fa7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1407a1736fe269e695a4f30fdd424b5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b70370d5986c0980fbe",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "a17cff1e45a18987f7aa",
+    "url": "./static/js/2.094b741e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.094b741e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b70370d5986c0980fbe",
+    "url": "./static/js/main.90080e5c.chunk.js"
+  },
+  {
+    "revision": "a502e626d8a4585a810d",
+    "url": "./static/js/runtime-main.ed7aa412.js"
+  }
+]);
